d12dd53a0b0331d2f00ea972eeb8a3f06d54e796
pratibhapw23 <pratibha_wabale@persistent.com>
2021-09-07 13:21:41 +0530

    Set driver properties in different file for reuse

---
+++ src/test/java/Utils/WebDriverProperties.java
@@ -0,0 +1,35 @@
+package Utils;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WebDriverProperties {
+	static WebDriver driver;
+	
+	public static WebDriver setChromeDriverProperties() throws IOException
+	{
+		WebDriverManager.chromedriver().setup();
+		  driver=new ChromeDriver();
+		 FileReader reader=new FileReader("Info.properties");
+		 Properties prop=new Properties();
+		 prop.load(reader); 
+		 String url=prop.getProperty("url");
+		 driver.get(url);
+		 driver.manage().window().maximize();
+		return driver;
+	}
+	public static void closeDriver()
+	{
+		driver.quit();
+	}
+	
+	
+
+}
