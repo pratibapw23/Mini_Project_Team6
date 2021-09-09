552039166eb8ab3c5f5f0eb981e3386092c89b8f
pratibhapw23 <pratibha_wabale@persistent.com>
2021-09-09 15:38:40 +0530

    Set properties for firefox driver

--- src/test/java/Utils/WebDriverProperties.java
+++ src/test/java/Utils/WebDriverProperties.java
@@ -6,6 +6,7 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -16,6 +17,18 @@ public class WebDriverProperties {
 	{
 		WebDriverManager.chromedriver().setup();
 		  driver=new ChromeDriver();
+		 FileReader reader=new FileReader("Info.properties");
+		 Properties prop=new Properties();
+		 prop.load(reader); 
+		 String url=prop.getProperty("url");
+		 driver.get(url);
+		 driver.manage().window().maximize();
+		return driver;
+	}
+	public static WebDriver setFirefoxDriverProperties() throws IOException
+	{
+		WebDriverManager.firefoxdriver().setup();
+		driver=new FirefoxDriver();
 		 FileReader reader=new FileReader("Info.properties");
 		 Properties prop=new Properties();
 		 prop.load(reader); 
