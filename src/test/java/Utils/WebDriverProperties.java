d9f1e8286490522cfd0f29b953e6883f2e071e56
pratibhapw23 <pratibha_wabale@persistent.com>
2021-09-12 12:04:43 +0530

    Added Edge Driver properties

--- src/test/java/Utils/WebDriverProperties.java
+++ src/test/java/Utils/WebDriverProperties.java
@@ -6,6 +6,7 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -17,6 +18,18 @@ public class WebDriverProperties {
 	{
 		WebDriverManager.chromedriver().setup();
 		  driver=new ChromeDriver();
+		 FileReader reader=new FileReader("Info.properties");
+		 Properties prop=new Properties();
+		 prop.load(reader); 
+		 String url=prop.getProperty("url");
+		 driver.get(url);
+		 driver.manage().window().maximize();
+		return driver;
+	}
+	public static WebDriver setEdgeDriverProperties() throws IOException
+	{
+		WebDriverManager.edgedriver().setup();
+		driver=new EdgeDriver();
 		 FileReader reader=new FileReader("Info.properties");
 		 Properties prop=new Properties();
 		 prop.load(reader); 
