161b7fe9711c41700f60ba3dc5b0a1922cee683f
Rutujadhav3 <rutuja_jadhav3@persistent.com>
2021-09-15 12:39:43 +0530

    Rutuja added Brower compatability test

---
+++ src/test/java/com/compatibilityTests/Home_03.java
@@ -0,0 +1,46 @@
+package com.compatibilityTests;
+
+import org.testng.annotations.Test;
+
+import Utils.ExcelUtils_Urls;
+import Utils.WebDriverProperties;
+
+import org.testng.annotations.BeforeMethod;
+
+import static org.testng.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+
+
+public class Home_03 {
+	WebDriver driver;
+  @Test
+  public void urlInDifferentBrowers() throws IOException {
+	  String ExceptedURL[]=ExcelUtils_Urls.getURLs();
+	  
+	 //Chrome
+	  driver = WebDriverProperties.setChromeDriverProperties();
+	  String URL_Chrome = driver.getCurrentUrl();
+	  assertEquals(ExceptedURL[0],URL_Chrome);
+	  System.out.println("URL verified on Chrome browser");
+	  driver.close();
+	  
+	//MicrosoftEdge
+	  driver = WebDriverProperties.setEdgeDriverProperties();
+	  String URL_Edge= driver.getCurrentUrl();
+	  assertEquals(ExceptedURL[0],URL_Edge);
+	  System.out.println("URL verified on Edge browser");
+	  driver.close();
+	 
+	 //FireFox
+	  /*driver = WebDriverProperties.setFirefoxDriverProperties();
+	  String URL_Firefox= driver.getCurrentUrl();
+	  assertEquals(ExceptedURL[0],URL_Firefox);
+	  System.out.println("URL verified on FireFox browser");
+	  driver.close();*/
+	  
+  }
+
+}
