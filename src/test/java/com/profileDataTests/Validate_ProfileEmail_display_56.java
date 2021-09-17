7be245bab05d96b94189ef5bdc874844a9916237
utkarshborawake <utkarshmb1999@gmail.com>
2021-09-17 19:42:23 +0530

    Utkarsh - profile email check

--- src/test/java/com/profileDataTests/Validate_ProfileEmail_display_56.java
+++ src/test/java/com/profileDataTests/Validate_ProfileEmail_display_56.java
@@ -1,4 +1,6 @@
 package com.profileDataTests;
+import static org.testng.Assert.assertEquals;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
@@ -11,39 +13,36 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import Utils.ExcelUtils;
 import Utils.WebDriverProperties;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import pageObjects.LoginPage;
+import pageObjects.ProfilePage;
 
 public class Validate_ProfileEmail_display_56 {
 	WebDriver driver;
 
   
   @Test//(groups= {"Login"})
-  public void Vrfy_EmailPresence() throws IOException, InterruptedException {
+  public void Vrfy_EmailVisibility() throws IOException, InterruptedException {
 	  
-	  driver.findElement(By.xpath("(//A[@href='login'][text()='Login'])[2]")).click();
-	  driver.findElement(By.xpath("(//INPUT[@class='input100'])[1]")).sendKeys("zxcv");
-	  driver.findElement(By.xpath("(//INPUT[@class='input100'])[2]")).sendKeys("Zxcv@123");
-	  driver.findElement(By.xpath("//BUTTON[@type='submit'][text()='\n"
-	  		+ "								Login\n"
-	  		+ "							']")).click();
+	  LoginPage.loginButton(driver).click();
+		 LoginPage.usernameTextBox(driver).sendKeys(ExcelUtils.getUsernameU());
+		 LoginPage.passwordTextBox(driver).sendKeys(ExcelUtils.getPasswordU());
+		 LoginPage.SignInButton(driver).submit();
+		 Thread.sleep(2000);
+		 
+		 ProfilePage.profileButton(driver).click();
 	  
-	  driver.findElement(By.xpath("//A[@href='profile'][text()='Profile']")).click();
-	  
-	  Boolean Display = driver.findElement(By.xpath("//DIV[@class='col-sm-9 text-secondary'][text()='\n"
-	  		+ "                          zxcv@gm.com\n"
-	  		+ "                        ']")).isDisplayed();
-	//To print the value
-	System.out.println("Element displayed is :"+Display);
-	  
-	  String ActualTitle = driver.getTitle();
-	  String ExpectedTitle = "Profile Page";
-	  Assert.assertEquals(ActualTitle, ExpectedTitle);
-  
-      System.out.println("Test Case Passed : Email Presence Verified");
+
+		 
+		 String ActualValue=ProfilePage.getActualEmail(driver).getText();
+		 assertEquals(ActualValue,"zxcv@gm.com");
+		 
+      System.out.println("Verified Email Visibility : "+ ActualValue);
       
 	  
-	  Thread.sleep(5000);
+	  Thread.sleep(1000);
 	  
 	  
   }
